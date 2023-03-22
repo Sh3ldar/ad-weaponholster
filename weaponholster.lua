c1a4fda05b193907a31023285e69482db4bf2321
Muzzy <126266752+FiveOPZ@users.noreply.github.com>
2023-03-22 22:25:24 +0000

    Update weaponholster.lua

--- weaponholster.lua
+++ weaponholster.lua
@@ -209,3 +209,13 @@ CreateThread(function()
 		end
 	end
 end)
+
+AddEventHandler('onResourceStart', function(resourceName)
+    if (GetCurrentResourceName() ~= resourceName) then
+        return
+    end
+    print("^4qb-weaponholster ^0|^2 Script started successfully.^7")
+    print("^4qb-weaponholster ^0|^5 Version: 1.0.4^7")
+    print("^4qb-weaponholster ^0|^2 If you need any help contact:^7")
+    print("^4qb-weaponholster ^0|^5 Muzzy#8363^7")
+end)
