70a4b210102da171ad65bcc18e3b3a8b05462476
Muzzy <126266752+FiveOPZ@users.noreply.github.com>
2023-03-24 16:42:08 +0000

    Update server.lua

--- server.lua
+++ server.lua
@@ -12,38 +12,23 @@ AddEventHandler('onResourceStart', function(resourceName)
     print("^4qb-weaponholster ^0|^5 Muzzy#8363^7")
 end)
 
--- Returns the current version set in fxmanifest.lua
 function GetCurrentVersion()
 	return GetResourceMetadata( GetCurrentResourceName(), "version" )
 end
 
--- Grabs the latest version number from the web GitHub
 PerformHttpRequest( "https://raw.githubusercontent.com/FiveOPZ/qb-weaponholster/main/version.txt", function( err, text, headers )
-	-- Wait to reduce spam
 	Citizen.Wait( 2000 )
 
-	-- Get the current resource version
 	local curVer = GetCurrentVersion()
+	
+	print( "^4qb-weaponholster ^0|^5 Current version: " .. curVer )
+	print( "^4qb-weaponholster ^0|^5 Latest recommended version: " .. text )
 
-	print( "  ||    Current version: " .. curVer )
-
-	if ( text ~= nil ) then
-		-- Print latest version
-		print( "qb-weaponholster | Latest recommended version: " .. text .."\n  ||" )
-
-		-- If the versions are different, print it out
-		if ( text ~= curVer ) then
-			print( "^4qb-weaponholster | ^0Version is outdated visit https://github.com/FiveOPZ/qb-weaponholster" )
-		else
-			print( "4qb-weaponholster | ^0Version is up to date" )
-		end
-	else
-		-- In case the version can not be requested, print out an error message
-		print( "4qb-weaponholster | ^0Error when trying to check version." )
-	end
-
-	-- Warn the console if the resource has been renamed, as this will cause issues with the resource's functionality.
 	if ( GetCurrentResourceName() ~= "qb-weaponholster" ) then
-		print( "4qb-weaponholster | ^0Resource name is not qb-weaponholster please change it." )
+		print( "^4qb-weaponholster ^0|^1 Resource name is not qb-weaponholster please change it!" )
+		print( "^4qb-weaponholster ^0|^1 Resource name is not qb-weaponholster please change it!" )
+		print( "^4qb-weaponholster ^0|^1 Resource name is not qb-weaponholster please change it!" )
+		print( "^4qb-weaponholster ^0|^1 Resource name is not qb-weaponholster please change it!" )
+		print( "^4qb-weaponholster ^0|^1 Resource name is not qb-weaponholster please change it!" )
 	end
 end )
