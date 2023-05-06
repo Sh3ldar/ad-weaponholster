c6105dfb285c9a725003398304041077a78724d2
Muzzy <126266752+FiveOPZ@users.noreply.github.com>
2023-05-06 17:41:50 +0100

    Update server.lua

--- server.lua
+++ server.lua
@@ -7,7 +7,7 @@ AddEventHandler('onResourceStart', function(resourceName)
         return
     end
     print("^4qb-weaponholster ^0|^2 Script started successfully.^7")
-    print("^4qb-weaponholster ^0|^5 Version: 1.0.7^7")
+    print("^4qb-weaponholster ^0|^5 Version: 1.0.8^7")
     print("^4qb-weaponholster ^0|^2 If you need any help contact:^7")
     print("^4qb-weaponholster ^0|^5 discord.gg/jCTza43^7")
 end)
